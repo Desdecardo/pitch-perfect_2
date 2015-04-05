9c072d9aed1ed5462c5415b2f67634297336d9ec
Desdecardo <maseo_sera@yahoo.com>
2015-04-04 21:19:34 -0600

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Phil Feinstein on 3/30/15.
+//  Copyright (c) 2015 Phil Feinstein. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
